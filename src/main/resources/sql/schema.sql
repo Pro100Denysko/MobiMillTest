4ac54327d73bd91c73095f7b84cc176537e768d8
pro100denysko <pro100denysko@gmail.com>
2018-06-26 21:46:17 +0300

    Update sql-scripts

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -2,29 +2,55 @@ DROP TABLE IF EXISTS company;
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS position;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS company_departments;
+DROP TABLE IF EXISTS user_departments;
 
 -- Table: company
 CREATE TABLE company (
-  id   INT         NOT NULL AUTO_INCREMENT,
+  id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL
 );
 
 -- Table: department
 CREATE TABLE department (
-  id   INT         NOT NULL AUTO_INCREMENT,
+  id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL
 );
 
 -- Table: position
 CREATE TABLE position (
-  id   INT         NOT NULL AUTO_INCREMENT,
+  id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL
 );
 
 -- Table: users
 CREATE TABLE users (
-  id            INT         NOT NULL AUTO_INCREMENT,
+  id            INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name          VARCHAR(45) NOT NULL,
   lastname      VARCHAR(45) NOT NULL,
   date_of_birth DATE        NOT NULL,
+  position_id   INT         NOT NULL,
+  company_id    INT         NOT NULL
+);
+
+-- Table for mapping company and departments: company_departments
+CREATE TABLE company_departments (
+  company_id    INT NOT NULL,
+  department_id INT NOT NULL,
+
+  UNIQUE (company_id, department_id),
+
+  FOREIGN KEY (company_id) REFERENCES company (id),
+  FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+-- Table for mapping user and departments: user_departments
+CREATE TABLE user_departments (
+  user_id       INT NOT NULL,
+  department_id INT NOT NULL,
+
+  UNIQUE (user_id, department_id),
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
