d399004c4cff9c55aad0533e626aa115f3c2a147
pro100denysko <pro100denysko@gmail.com>
2018-06-26 16:59:23 +0300

    Add application.properties

---
+++ src/main/java/ua/com/app/model/User.java
@@ -0,0 +1,63 @@
+package ua.com.app.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @NotNull
+  @Column(name = "NAME")
+  private String name;
+
+  @NotNull
+  @Column(name = "LASTNAME")
+  private String lastname;
+
+  @NotNull
+  @Column(name = "DATE_OF_BIRTH")
+  private Date dateOfBirth;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public Date getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(Date dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+}
