acc4de66be9c6a99bf66d827462792ff4326e0a3
pro100denysko <pro100denysko@gmail.com>
2018-06-27 14:07:51 +0300

    Add Optional

--- src/main/java/ua/com/app/DAO/CompanyRepository.java
+++ src/main/java/ua/com/app/DAO/CompanyRepository.java
@@ -1,10 +1,11 @@
 package ua.com.app.DAO;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.app.model.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-  Company findById(Long id);
+  Optional<Company> findById(Long id);
 
 }
