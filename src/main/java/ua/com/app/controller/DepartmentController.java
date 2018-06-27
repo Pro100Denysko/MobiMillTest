cfb5fc55769719a8585db7ac4ed3335e3aa3a95d
pro100denysko <pro100denysko@gmail.com>
2018-06-27 16:32:55 +0300

    Add addDepartmentToCompany

--- src/main/java/ua/com/app/controller/DepartmentController.java
+++ src/main/java/ua/com/app/controller/DepartmentController.java
@@ -1,13 +1,15 @@
 package ua.com.app.controller;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ua.com.app.model.Company;
 import ua.com.app.model.Department;
+import ua.com.app.service.CompanyService;
 import ua.com.app.service.DepartmentService;
 
 @RestController
@@ -15,16 +17,25 @@ import ua.com.app.service.DepartmentService;
 public class DepartmentController {
 
   private DepartmentService departmentService;
+  private CompanyService companyService;
 
   @Autowired
-  public DepartmentController(DepartmentService departmentService) {
+  public DepartmentController(DepartmentService departmentService,
+      CompanyService companyService) {
     this.departmentService = departmentService;
+    this.companyService = companyService;
   }
 
-  @RequestMapping(value = "/departments", method = RequestMethod.GET)
-  public ResponseEntity<List<Department>> getAllDepartments() {
-    List<Department> listOfDepartments = departmentService.getAllDepartments();
-    return ResponseEntity.status(HttpStatus.OK).body(listOfDepartments);
-  }
+  @RequestMapping(value = "/company/{id}/departments/{depId}", method = RequestMethod.PUT)
+  public ResponseEntity<Company> addDepartmentToCompany(@PathVariable("id") Long compId,
+      @PathVariable("depId") Long depId) {
 
+    Department newDepartment = departmentService.getById(depId);
+    System.out.println(newDepartment.getId());
+    Company company = companyService.getById(compId);
+    System.out.println(company.getId());
+
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(companyService.addDepartment(company, newDepartment));
+  }
 }
