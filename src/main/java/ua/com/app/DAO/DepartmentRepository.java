3ec70b8395b87c7857501959579ea90df3e4fb72
D. Bondarenko <pro100denysko@gmail.com>
2018-06-30 19:44:07 +0300

    Add Optional

--- src/main/java/ua/com/app/DAO/DepartmentRepository.java
+++ src/main/java/ua/com/app/DAO/DepartmentRepository.java
@@ -6,6 +6,6 @@ import ua.com.app.model.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
-  Optional<Department> getById(Long id);
+  Optional<Department> findById(Long id);
 
 }
