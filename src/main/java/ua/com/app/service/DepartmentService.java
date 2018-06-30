209f53178904b99c1b2426b7f27f0b867c0a2be8
D. Bondarenko <pro100denysko@gmail.com>
2018-06-30 19:50:10 +0300

    Add Deprecated methods

--- src/main/java/ua/com/app/service/DepartmentService.java
+++ src/main/java/ua/com/app/service/DepartmentService.java
@@ -1,9 +1,12 @@
 package ua.com.app.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.com.app.DAO.DepartmentRepository;
+import ua.com.app.model.Company;
 import ua.com.app.model.Department;
+import ua.com.app.model.User;
 
 @Service
 public class DepartmentService {
@@ -16,7 +19,26 @@ public class DepartmentService {
   }
 
   public Department getById(Long id) {
-    return departmentRepository.getById(id).orElseThrow(() ->
+    return departmentRepository.findById(id).orElseThrow(() ->
         new RuntimeException("Requested entity was not found"));
   }
+
+  @Deprecated
+  public void deleteDepartment(Company company, Department departmentToDelete) {
+    Department department = departmentRepository.findById(departmentToDelete.getId())
+        .orElseThrow(() ->
+            new RuntimeException("Requested entity was not found"));
+    department.getCompanies().remove(company);
+    departmentRepository.save(department);
+  }
+
+  @Deprecated
+  public void addDepartment(Company companyToUpdate, Department newDepartment) {
+    Department department = departmentRepository.findById(newDepartment.getId())
+        .orElseThrow(() ->
+            new RuntimeException("Requested entity was not found"));
+    department.getCompanies().add(companyToUpdate);
+  }
+
+
 }
